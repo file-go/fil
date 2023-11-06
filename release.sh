5cb02ac9a06c76bee5caf940d3add69ce24503f2
Joeky <joeky5888@gmail.com>
2023-11-07 04:20:28 +0800

    Add github action
    Signed-off-by: Joeky <joeky5888@gmail.com>

--- release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o fil-linux-x64
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o fil-linux-arm64
-CGO_ENABLED=0 GOOS=linux GOARCH=arm   go build -o fil-linux-arm
-
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o fil-win-x64.exe
-
-CGO_ENABLED=0 GOOS=darwin  GOARCH=amd64 go build -o fil-osx-x64
